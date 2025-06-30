90704e8e7dbb3949821d5971092129e4c4f849be
Antonio-Pocomento <pocomento.antonio@gmail.com>
2025-06-30 13:35:32 +0200

    Inizio Javadoc

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -6,6 +6,20 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 
+/**
+ * La classe {@code Home} rappresenta l'interfaccia grafica principale dell'applicazione.
+ * Consente all'utente di scegliere se effettuare il login, registrarsi o uscire dall'applicazione.
+ *
+ * <p>Questa schermata contiene tre pulsanti:
+ * <ul>
+ *     <li><b>Login</b>: apre la finestra di login.</li>
+ *     <li><b>Register</b>: apre la finestra di registrazione.</li>
+ *     <li><b>Exit</b>: chiude l'applicazione.</li>
+ * </ul>
+ *
+ * <p>Viene utilizzato un {@link Controller} per gestire la logica applicativa.
+ *
+ */
 public class Home {
     private JPanel homePanel;
     private JButton exitButton;
@@ -15,13 +29,18 @@ public class Home {
     private static JFrame frame;
     private final Controller controller;
 
+    /**
+     * Costruttore della classe {@code Home}.
+     * Inizializza i componenti grafici, imposta icone e bordi,
+     * e definisce le azioni associate ai pulsanti.
+     */
     public Home() {
         controller = new Controller();
         loginIcon.setIcon(ImageLoader.loadIcon("src/main/images/airplaneIcon.png"));
-        UtilFunctionsForGUI.setupLayoutAndBackground(frame,homePanel);
-        registerButton.setBorder(new LineBorder(Color.black,3,false));
-        loginButton.setBorder(new LineBorder(Color.black,3,false));
-        exitButton.setBorder(new LineBorder(Color.black,3,false));
+        UtilFunctionsForGUI.setupLayoutAndBackground(frame, homePanel);
+        registerButton.setBorder(new LineBorder(Color.black, 3, false));
+        loginButton.setBorder(new LineBorder(Color.black, 3, false));
+        exitButton.setBorder(new LineBorder(Color.black, 3, false));
         UtilFunctionsForGUI.addHoverEffect(exitButton);
         UtilFunctionsForGUI.addHoverEffect(registerButton);
         UtilFunctionsForGUI.addHoverEffect(loginButton);
@@ -42,6 +61,12 @@ public class Home {
         });
     }
 
+    /**
+     * Metodo principale di avvio dell'applicazione.
+     * Crea e visualizza la finestra della schermata home.
+     *
+     * @param args argomenti della riga di comando (non utilizzati)
+     */
     public static void main(String[] args) {
         System.setProperty("sun.java2d.uiScale", "1.0");
 
